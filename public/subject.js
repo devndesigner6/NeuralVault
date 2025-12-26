a02045c380497cafd42d20e5e30b526942484901
devndesigner6 <peddadahemanth6@gmail.com>
2025-12-26 15:41:13 +0530

    Remove typography section from homepage and fix subject.js JSON loading with better error messages

--- public/subject.js
+++ public/subject.js
@@ -8,80 +8,80 @@ $(document).ready(function () {
         ? ""
         : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
+    
+    function casing(string) {
+      return string.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
+        return a.toUpperCase();
+      });
+    }
+    
     var codeValue = getUrlParameter("code");
     console.log("code param:", codeValue);
     if (!codeValue) {
-      $('h2').text('Error fetching details');
+      $('h2').text('Error: No subject selected');
       $('table').hide();
       console.error('Missing ?code= param in URL');
       return;
     }
+    
     $(".title").text(codeValue);
-    // Try relative path first, then absolute
-    var filePath = "./info/" + codeValue + ".json";
-    console.log("Attempting to load:", filePath);
-    $.getJSON(filePath, function (data) {
-      var fileName = casing(data[0].name);
+    var filePath = "/info/" + codeValue + ".json";
+    console.log("Loading subject JSON from:", filePath);
+    
+    $.ajax({
+      url: filePath,
+      dataType: 'json',
+      success: function(data) {
+        var fileName = casing(data[0].name);
+        $(".title").text(data[0].code + ": " + fileName);
 
-      function casing(string) {
-        return string.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
-          return a.toUpperCase();
+        var unitTableBody = $("#unitTableBody");
+        $.each(data[0].units, function (index, unit) {
+          var row = $("<tr></tr>");
+          row.append("<td>" + casing(unit.name) + "</td>");
+          if (data[0].syllabus == true) {
+            $('th:contains("Syllabus")').remove();
+          }
+          !data[0].syllabus &&
+            row.append(
+              '<td><a class="unitLink" data-index="' +
+                index +
+                '">' +
+                "View Syllabus</a></td>"
+            );
+          unitTableBody.append(row);
         });
-      }
-      console.log(data);
-      console.log(data);
-      console.log(fileName);
-      $(".title").text(data[0].code + ": " + fileName);
-
-      var unitTableBody = $("#unitTableBody");
-      $.each(data[0].units, function (index, unit) {
-        var row = $("<tr></tr>");
-        row.append("<td>" + casing(unit.name) + "</td>");
-        if (data[0].syllabus == true) {
-          $('th:contains("Syllabus")').remove();
-        }
-        !data[0].syllabus &&
-          row.append(
-            '<td><a class="unitLink" data-index="' +
-              index +
-              '">' +
-              "View Syllabus</a></td>"
-          );
-        unitTableBody.append(row);
-      });
-      $(".unitLink").click(function () {
-        var index = $(this).data("index");
-        var unit = data[0].units[index];
-        var unitInfo = $("#unitInfo");
-        unitInfo.empty();
-        unitInfo.append("<h3>" + unit.name + "</h3>");
-        var topicsList = $("<ul></ul>");
-        $.each(unit.topics, function (index, topic) {
-          var topicItem = $("<li>" + topic.topic + "</li>");
-          var subTopicsList = $("<ul></ul>");
-          $.each(topic.subTopics, function (index, subTopic) {
-            subTopicsList.append("<li>" + subTopic + "</li>");
+        
+        $(".unitLink").click(function () {
+          var index = $(this).data("index");
+          var unit = data[0].units[index];
+          var unitInfo = $("#unitInfo");
+          unitInfo.empty();
+          unitInfo.append("<h3>" + unit.name + "</h3>");
+          var topicsList = $("<ul></ul>");
+          $.each(unit.topics, function (index, topic) {
+            var topicItem = $("<li>" + topic.topic + "</li>");
+            var subTopicsList = $("<ul></ul>");
+            $.each(topic.subTopics, function (index, subTopic) {
+              subTopicsList.append("<li>" + subTopic + "</li>");
+            });
+            topicItem.append(subTopicsList);
+            topicsList.append(topicItem);
           });
-          topicItem.append(subTopicsList);
-          topicsList.append(topicItem);
+          unitInfo.append(topicsList);
+          $(".info").fadeIn();
+          var infoElement = $(".info");
+          $("html, body").animate(
+            {
+              scrollTop: infoElement.offset().top,
+            },
+            200
+          );
         });
-        unitInfo.append(topicsList);
-        $(".info").fadeIn();
-        var infoElement = $(".info");
-        $("html, body").animate(
-          {
-            scrollTop: infoElement.offset().top,
-          },
-          200
-        );
-      });
 
-      if (data[0].links) {
-        $.each(data[0].links, function (index, linkObj) {
-          $(".resources").slideDown();
-
-          // Resolve resource links to absolute /resources path
-          (function () {
+        if (data[0].links) {
+          $.each(data[0].links, function (index, linkObj) {
+            $(".resources").slideDown();
             var rawLink = String(linkObj.link || "");
             // Convert ./resources/... to /resources/...
             var resolvedHref = rawLink.replace(/^\.\//, "/");
@@ -95,92 +95,15 @@ $(document).ready(function () {
                 '" target="_blank" rel="noopener noreferrer">view</a></p>'
             );
             $(".resources").append(link);
-          })();
-        });
-      } else {
+          });
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error('Failed to load subject JSON:', textStatus, errorThrown);
+        console.error('Status code:', jqXHR.status);
+        console.error('Tried URL:', filePath);
+        $('h2').text('Error loading subject: ' + errorThrown);
+        $('table').hide();
       }
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-            console.error('Primary relative path load failed:', textStatus, errorThrown, jqXHR && jqXHR.status);
-            console.log('Falling back to absolute path /info/...');
-            var altPath = "/info/" + codeValue + ".json";
-            $.getJSON(altPath, function (data) {
-              // Re-run the same rendering logic by simulating success
-              var fileName = (function casing(string) {
-                return string.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
-                  return a.toUpperCase();
-                });
-              })(data[0].name);
-              $(".title").text(data[0].code + ": " + fileName);
-              var unitTableBody = $("#unitTableBody");
-              $.each(data[0].units, function (index, unit) {
-                var row = $("<tr></tr>");
-                row.append("<td>" + (function (string){return string.toLowerCase().replace(/(?:^|\s)\S/g,function(a){return a.toUpperCase();});})(unit.name) + "</td>");
-                if (data[0].syllabus == true) {
-                  $('th:contains("Syllabus")').remove();
-                }
-                !data[0].syllabus &&
-                  row.append(
-                    '<td><a class="unitLink" data-index="' +
-                      index +
-                      '">' +
-                      "View Syllabus</a></td>"
-                  );
-                unitTableBody.append(row);
-              });
-              $(".unitLink").click(function () {
-                var index = $(this).data("index");
-                var unit = data[0].units[index];
-                var unitInfo = $("#unitInfo");
-                unitInfo.empty();
-                unitInfo.append("<h3>" + unit.name + "</h3>");
-                var topicsList = $("<ul></ul>");
-                $.each(unit.topics, function (index, topic) {
-                  var topicItem = $("<li>" + topic.topic + "</li>");
-                  var subTopicsList = $("<ul></ul>");
-                  $.each(topic.subTopics, function (index, subTopic) {
-                    subTopicsList.append("<li>" + subTopic + "</li>");
-                  });
-                  topicItem.append(subTopicsList);
-                  topicsList.append(topicItem);
-                });
-                unitInfo.append(topicsList);
-                $(".info").fadeIn();
-                var infoElement = $(".info");
-                $("html, body").animate(
-                  {
-                    scrollTop: infoElement.offset().top,
-                  },
-                  200
-                );
-              });
-
-              if (data[0].links) {
-                $.each(data[0].links, function (index, linkObj) {
-                  $(".resources").slideDown();
-                  (function () {
-                    var rawLink = String(linkObj.link || "");
-                    var normalized = rawLink.replace(/^\.\//, "./");
-                    var resolvedHref;
-                    try {
-                      resolvedHref = new URL(normalized, window.location.href).href;
-                    } catch (e) {
-                      resolvedHref = encodeURI(normalized);
-                    }
-                    var link = $(
-                      "<p>" +
-                        linkObj["name"] +
-                        ': <a href="' +
-                        resolvedHref +
-                        '" target="_blank" rel="noopener noreferrer">view</a></p>'
-                    );
-                    $(".resources").append(link);
-                  })();
-                });
-              }
-            }).fail(function(jqXHR2, textStatus2, errorThrown2) {
-              $('h2').text('Error fetching details');
-              $('table').hide();
-              console.error('Alt info load failed:', textStatus2, errorThrown2, jqXHR2 && jqXHR2.status);
-            });
-        });
-  });
+    });
+});
